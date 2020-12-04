9335fcf047e486ddda7f3e44330294973a879e81
Skurik <skurskii.sa@students.dvfu.ru>
2020-12-04 18:54:55 +1000

    a

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,7 +9,13 @@ use http\Env\Request;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class ProductController extends AbstractController
 {
@@ -40,22 +46,90 @@ class ProductController extends AbstractController
         return new Response('Saved new product with id '.$product->getId());
     }
 
+//    /**
+//     * @Route("/product/{id}", name="product_show")
+//     */
+//    public function show(int $id): Response
+//    {
+//        $product = $this->getDoctrine()
+//            ->getRepository(Product::class)
+//            ->find($id);
+//
+//        if (!$product) {
+//            throw $this->createNotFoundException(
+//                'No product found for id '.$id
+//            );
+//        }
+//
+//        return new Response('Check out this great product: '.$product->getName());
+//
+//        // or render a template
+//        // in the template, print things with {{ product.name }}
+//        // return $this->render('product/show.html.twig', ['product' => $product]);
+//    }
+
     /**
      * @Route("/product/{id}", name="product_show")
      */
-
-    public function show(int $id): Response
+    public function show(Product $product): Response
     {
-        $product = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->find($id);
 
         if (!$product) {
             throw $this->createNotFoundException(
-                'No product found for id ' . $id
+                'No product found for id ' . $product.id
             );
         }
 
         return new Response('Check out this great product: ' . $product->getName());
+    }
+
+    /**
+     * @Route("/product/edit/{id}")
+     */
+
+    public function update(int $id): Response{
+        $entityManager = $this->getDoctrine()->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if(!$product)
+        {
+            throw $this->createNotFoundException(
+                'No product found for id'.$id
+            );
+        }
+
+        $product->setName('Snickers');
+        $product->setPrice(2000);
+        $product->setDescription('Nike');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+
+    }
+
+    /**
+     * @Route("/product/delete/{id}")
+     */
+
+    public function delete(int $id): Response{
+        $entityManager = $this->getDoctrine()->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if(!$product)
+        {
+            throw $this->createNotFoundException(
+                'No product found for id'.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return new Response(
+            'Product '.$product->getName().' sold'
+        );
+
     }
 }
